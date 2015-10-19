0a211a8ce99b27c2e1bf721334bed2ec56a87047
Frederico <fred@cs.utah.edu>
2015-10-19 13:59:35 -0600

    Stanford NER added

--- src/boilerplate/Question.java
+++ src/boilerplate/Question.java
@@ -11,11 +11,29 @@ public class Question {
 		WHICH, WHY, WHOSE, WHAT, HOW, WHEN, WHERE, WHO;
 	};
 
+	/**
+	 * Creates a typed question
+	 * based on a string given
+	 */
 	public Question(String question) {
 		this.question = question;
 		types = new ArrayList<Question.Type>();
 	}
 
+	/**
+	 * Generate a Bag-of-Words
+	 */
+	private void bagOfWords(){
+		
+	}
+	
+	/**
+	 * Return the type of the question
+	 */
+	public Type getType() {
+		return types.get(0); //TODO: Allow multiple types
+	}
+	
 	/* Getters and Setters */
 	public String getQuestion() {
 		return question;
@@ -23,12 +41,5 @@ public class Question {
 
 	public void addType(Type type) {
 		types.add(type);
-	}
-
-	/**
-	 * Return the type of the question
-	 */
-	public Type getType() {
-		return types.get(0);
 	}
 }
