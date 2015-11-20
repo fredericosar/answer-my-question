0d81a86deb5afb92cef9f7275032976e16ebfbaf
Owner <Owner@owner-ec94ca4f7.uconnect.utah.edu>
2015-11-20 12:59:36 -0700

    Just added a template java file "WhoseAnswerer.java".  No additional code was put inside.

---
+++ src/answer/WhoseAnswerer.java
@@ -0,0 +1,31 @@
+package answer;
+
+import java.util.ArrayList;
+
+import logic.StoryClassifier;
+import boilerplate.*;
+import rules.Scores;
+import rules.AnswerRules.AType;
+
+public class WhoseAnswerer {
+
+	private Question question;
+	private Story story;
+	private StoryClassifier sc;
+	private ArrayList<Integer> scores;
+
+	public WhoseAnswerer(Question question, Story story, StoryClassifier sc, ArrayList<Integer> scores) {
+		this.question = question;
+		this.story = story;
+		this.sc = sc;
+		this.scores = scores;
+	}
+
+	/**
+	 * Answer the WHOSE question
+	 */
+	public void answer(){
+		String bestSentence = story.getSentence(CommonAnswerer.findBest(scores));
+		System.out.println(bestSentence);
+	}
+}
